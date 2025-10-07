25540cac39ddde4498f065430df78de1701c0e02
MiliN <milinpatel1118@gmail.com>
2025-10-07 13:40:21 +0530

    first commit

---
+++ ass_1033_3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,num;
+    printf("enter the number :-");
+    scanf("%d",&num);
+    if(a%2==0)
+    {
+        printf("%d is even",a);
+    }
+    else
+    {
+        printf("%d is odd",a);
+    }
+   
+}
+
+
